2ae4fba87bb864a261d2b731e0d41be84d213291
sreyasProgrammer <sreyassatheesh755@gmail.com>
2022-12-16 00:24:32 +0530

    js is added to a separate file (customer and seller manage pages)

--- admin/manage-customer.php
+++ admin/manage-customer.php
@@ -16,6 +16,7 @@ if ($_SESSION['admin-id'] != 1) {
     <link rel="stylesheet" href="../css/main.css">
     <link rel="stylesheet" href="css/cu-ma.css">
     <script src='../jquery/jquery.js'></script>
+    <script src='js/manage-customer.js' defer></script>
 </head>
 
 <body class="real-body">
@@ -47,93 +48,7 @@ if ($_SESSION['admin-id'] != 1) {
         </div>
     </div>
     <div class="body admin-body">
-
+        <!-- //-data comes here  -->
     </div>
-    <script>
-        //-search functionalities : START
-
-        function search(item){
-            const cards = document.querySelectorAll(".customer-card-container")
-            Array.prototype.forEach.call(cards, (card) => {
-                card.setAttribute('show',false)
-            })
-            let i, j
-            const text = item.value.toLowerCase()
-            const names = document.querySelectorAll(".name")
-            Array.prototype.forEach.call(names, (name) => {
-                let check = true
-                j = 0
-                nameText = name.innerHTML.toLowerCase()
-                for(i=0; i<text.length; i++){
-                    if(nameText.includes(text[i], j)){
-                        j = nameText.indexOf(text[i], j) + 1
-                        continue
-                    }
-                    check = false
-                    break
-                }
-                if(check == true){
-                    let c = name.parentElement.parentElement.parentElement
-                    c.setAttribute('show',true)
-                }
-            })
-        }
-        let searchI = document.querySelector(".search-clear")
-        searchI.addEventListener("click", () =>{
-            const ch = document.querySelector(".btn-container").children
-            Array.prototype.forEach.call(ch, (btn, i) => {
-                if((btn.getAttribute("selected")) === 'true') {
-                    if(i == 0) getUsers (1)
-                    else getUsers(2)
-                }
-            })
-        })
-
-        //-search functionalities : ENDS 
-
-        function btnClicked(value){
-            const ch = document.querySelector(".btn-container").children
-            console.log(ch)
-            Array.prototype.forEach.call(ch, (btn) => {
-                btn.setAttribute("selected",false)
-            })
-            ch[value-1].setAttribute("selected",true)
-            if(value == 1) getUsers(1)
-            else getUsers(2)
-        }
-        getUsers(1)
-        function getUsers(value){
-            $.ajax({
-                url:'php/get-users.php',
-                type:'POST',
-                data: {wh: value},
-                success: (data) => {
-                    $(".body").html(data)
-                }
-            })
-        }
-        function removeUser(item){
-            const id = item.getAttribute("id");
-            $.ajax({
-                url:'php/remove-user.php',
-                type:'POST',
-                data: {id : id},
-                success: () => {
-                    getUsers(1)
-                }
-            })
-        }
-        function undoRemove(item){
-            const id = item.getAttribute("id");
-            $.ajax({
-                url:'php/undo-remove.php',
-                type:'POST',
-                data: {id : id},
-                success: () => {
-                    getUsers(2)
-                }
-            })
-        }
-    </script>
 </body>
 </html>
--- admin/manage-seller.php
+++ admin/manage-seller.php
@@ -17,6 +17,7 @@ if ($_SESSION['admin-id'] != 1) {
     <link rel="stylesheet" href="css/se-ma.css">
     <link rel="stylesheet" href="../css/load.css">
     <script src="../jquery/jquery.js"></script>
+    <script src="js/manage-seller.js" defer></script>
 </head>
 
 <body class="real-body">
@@ -52,134 +53,8 @@ if ($_SESSION['admin-id'] != 1) {
         </div>
     </div>
     <div class="body admin-body">
-
+        <!--//-data comes here  -->
     </div>
-    <script>
-    //-search functionalities : START
-
-    function search(item){
-        const cards = document.querySelectorAll(".seller-card-request-container")
-        Array.prototype.forEach.call(cards, (card) => {
-            card.setAttribute('show',false)
-        })
-        const cards2 = document.querySelectorAll(".seller-card-container")
-        Array.prototype.forEach.call(cards2, (card) => {
-            card.setAttribute('show',false)
-        })
-        let i, j
-        const text = item.value.toLowerCase()
-        const names = document.querySelectorAll(".name")
-        Array.prototype.forEach.call(names, (name) => {
-            let check = true
-            j = 0
-            nameText = name.innerHTML.toLowerCase()
-            for(i=0; i<text.length; i++){
-                if(nameText.includes(text[i], j)){
-                    j = nameText.indexOf(text[i], j) + 1
-                    continue
-                }
-                check = false
-                break
-            }
-            if(check == true){
-                let c = name.parentElement.parentElement.parentElement
-                c.setAttribute('show',true)
-            }
-        })
-    }
-    
-    let searchI = document.querySelector(".search-clear")
-    searchI.addEventListener("click", () =>{
-        const ch = document.querySelector(".btn-container").children
-        Array.prototype.forEach.call(ch, (btn, i) => {
-            if((btn.getAttribute("selected")) === 'true') {
-                if(i == 0) getRequest()
-                else if(i == 1) getSellers (1)
-                else getSellers(2)
-            }
-        })
-    })
-
-    //-search functionalities : ENDS 
-
-
-    function btnClicked(value){
-        const ch = document.querySelector(".btn-container").children
-        Array.prototype.forEach.call(ch, (btn) => {
-            btn.setAttribute("selected",false)
-        })
-        ch[value].setAttribute("selected",true)
-        if(value == 0) getRequest()
-        else if(value == 1) getSellers(1)
-        else getSellers(2)
-    }
-    getRequest()
-    function getRequest(){
-        $.ajax({
-            url:'php/get-request.php',
-            type:'POST',
-            success: (data) => {
-                $(".body").html(data)
-            }
-        })
-    }
-    function getSellers(value){
-        $.ajax({
-            url:'php/get-sellers.php',
-            type:'POST',
-            data: {wh: value},
-            success: (data) => {
-                $(".body").html(data)
-            }
-        })
-    }
-    function removeSeller(item){
-        const id = item.getAttribute("id");
-        $.ajax({
-            url:'php/remove-seller.php',
-            type:'POST',
-            data: {id : id},
-            success: () => {
-                getSellers(1)
-            }
-        })
-    }
-    function acceptOrReject(item, value){
-        const id = item.getAttribute("id")
-        value = (value == 1) ? 1 : -1
-        if(value == 1){
-            $.ajax({
-                url:'php/accept-or-reject-request.php',
-                type:'POST',
-                data: {id : id, value: value},
-                success: () => {
-                    getRequest()
-                }
-            })
-        } else {
-           $.ajax({
-                url:'php/accept-or-reject-request.php',
-                type:'POST',
-                data: {id : id, value: value},
-                success: () => {
-                    getRequest()
-                }
-            })  
-        }
-    }
-    function undoRemove(item){
-        const id = item.getAttribute("id");
-        $.ajax({
-            url:'php/undo-remove-seller.php',
-            type:'POST',
-            data: {id : id},
-            success: () => {
-                getSellers(2)
-            }
-        })
-    }
-    
-    </script>
 </body>
 
 </html>
